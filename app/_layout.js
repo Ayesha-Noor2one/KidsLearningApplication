9e83686cf65e98592efa796220f02a4fc86d1ac3
ayesha-noor2one <an5765331@gmail.com>
2025-09-01 14:03:01 -0700

    disable navigation in

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import { BackHandler } from "react-native";
 import { Stack } from "expo-router";
 import { UsageTimerProvider } from "./UsageTimerContext";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      () => {
+        return true; // 🔒 block karega har jagah
+      }
+    );
+
+    return () => backHandler.remove(); // cleanup
+  }, []);
   return (
     <UsageTimerProvider>
       <Stack screenOptions={{ headerShown: false }} />
